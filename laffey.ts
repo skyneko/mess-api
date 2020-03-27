b936e4fc1990192d093900f3fdc8708ca5d613f9
yui421 <banhgao.nya@gmail.com>
2020-03-27 15:12:04 +0700

    add: sendMsg

--- laffey.ts
+++ laffey.ts
@@ -1,10 +1,33 @@
-import { login, User, UserRequestData, saveCookie, getUIDFromCookie, listen, Message } from "./src/index"
 import { readFileSync } from "fs";
+import {
+    login,
+    User,
+    UserRequestData,
+    saveCookie,
+    listen,
+    Message,
+    Messenger,
+    MessengerApi
+} from "./src/index"
 
 const user: User = JSON.parse(readFileSync("./user/config.json", "utf-8"))
 
 login(user)
     .then(saveCookie)
     .then((data: UserRequestData) => {
-        listen(data, (msg: Message) => { console.log(msg.messageMetadata.actorFbId, msg.body) })
+        const bot = new Messenger(data)
+        listen(data, (msg: Message) => hadleMessage(msg, new Messenger(data)))
     })
+
+
+function hadleMessage(msg: Message, Bot: MessengerApi) {
+    console.log(msg.messageMetadata.actorFbId, msg.body)
+    if (msg.messageMetadata.actorFbId !== "100041576433270") {
+
+        if (msg.body === "ping")
+            Bot.sendMsg("pong", parseInt(msg.messageMetadata.threadKey.threadFbId))
+
+        if (msg.body === "uwu")
+            Bot.sendMsg("owo", parseInt(msg.messageMetadata.threadKey.threadFbId))
+    }
+}
